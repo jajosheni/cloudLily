2f32d2882b9b760fa2385e53278e9f936c373d73
Sheni Hamitaj <shen.i@live.com>
2019-05-26 06:55:57 +0300

    tomorrow Land

--- public/javascripts/newAdvert.js
+++ public/javascripts/newAdvert.js
@@ -6,4 +6,8 @@ document.querySelector('#advert-form').addEventListener('reset', function (event
     document.querySelector("#dummyframe").style.display='none';
 });
 
-document.querySelector("#advert-duration").valueAsDate = new Date();
+let today = new Date();
+let tomorrow = new Date();
+tomorrow.setDate(today.getDate()+1);
+
+document.querySelector("#advert-duration").valueAsDate = tomorrow;
